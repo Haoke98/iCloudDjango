d446246af558c3ee0f5502628f59cf08aaca526e
Sadam·Sadik <1903249375@qq.com>
2024-03-31 20:01:01 +0800

    重新调整了目录结构, admin.py文件, 按照Admin模型, 切割成挨个Admin模型文件并打包进admin包里
    Signed-off-by: Sadam·Sadik <1903249375@qq.com>

--- icloud/admin/__init__.py
+++ icloud/admin/__init__.py
@@ -7,24 +7,9 @@
 @disc:
 ======================================="""
 from .account import AccountAdmin
-import os.path
-import threading
-import urllib.parse
-import uuid
-from datetime import datetime
-from urllib.parse import urlencode
-
-from django.contrib import admin
-from django.http import JsonResponse
-from minio_storage.storage import get_setting
-from simplepro.admin import FieldOptions
-from simplepro.decorators import button, layer
-from simplepro.dialog import MultipleCellDialog, ModalDialog
-from simpleui.admin import AjaxAdmin
-
-from utils import human_readable_bytes, human_readable_time, icloud
-from ..models import IMedia, Album, LocalMedia, AppleId
-from ..services import collect_all_medias, delete_from_icloud, migrateIcloudToLocal
+from .album import AlbumAdmin
+from .iMedia import IMediaAdmin
+from .localMedia import LocalMediaAdmin
 
 
 
