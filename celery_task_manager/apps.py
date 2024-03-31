85e03dc001f743839995d8beb5dc57d3f4e3641b
Sadam·Sadik <1903249375@qq.com>
2024-03-31 19:43:26 +0800

    移除了celery_task_manager用依赖django_celery_stack来代替,实现模块化加载
    Signed-off-by: Sadam·Sadik <1903249375@qq.com>

--- celery_task_manager/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class CeleryTaskManagerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'celery_task_manager'
-    verbose_name = _('Task Management')
